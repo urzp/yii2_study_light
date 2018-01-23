1a569e7640f2198ee6439972fdee98c1d1e5aa0f
urzp <urzp000@gmail.com>
2018-01-23 20:00:50 +0300

    4 controllers id = test default

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -9,6 +9,7 @@ class MyController extends Controller
 	 
 	public function actionIndex($id = null){
 		$hi = "hello, world";
+		if (!$id) $id = "test";
 		$names = ['Ivanov', 'Petrov', 'Sidorov'];
 		// return $this->render('index',['hello'=> $hi, 'names' => $names ]);
 		return $this->render('index',compact('hi', 'names', 'id'));
