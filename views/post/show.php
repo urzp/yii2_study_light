ed4a98d2fe92e74c1f4bfe646186a13c71a2a4ad
urzp <urzp000@gmail.com>
2018-01-29 19:38:23 +0300

    Study_7 add POS_LOAD for registerJS function

--- views/post/show.php
+++ views/post/show.php
@@ -3,4 +3,4 @@
 <?php // $this->registerJsFile('@web/js/script.js',['depends' => 'yii\web\YiiAsset']) ?> 
 
 
-<?php $this->registerJs( " $('.conteiner').append('<p>SHOW !!!!</p>'); ") ?>
+<?php $this->registerJs( " $('.conteiner').append('<p>SHOW !!!!</p>'); ", \yii\web\View::POS_LOAD) ?>
