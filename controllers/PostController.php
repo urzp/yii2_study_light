ad0b9bab0b3e01d33f7e6b1db9e109bc0c5b7958
urzp <urzp000@gmail.com>
2018-01-29 22:56:25 +0300

    Study_7 add block of js code with ajax request

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -10,7 +10,10 @@ class PostController extends AppController
 	public $layout = 'Basic';
 
 	public function actionIndex(){
-
+		if(Yii::$app->request->isAjax){
+			debug($_GET);
+			return 'test';
+		}
 		return $this -> render('test');
 	}
 
--- views/post/show.php
+++ views/post/show.php
@@ -5,4 +5,30 @@
 
 <?php // $this->registerJs( " $('.conteiner').append('<p>SHOW !!!!</p>'); ", \yii\web\View::POS_LOAD) ?>
 
-<?php  $this->registerCss('.conteiner{ background: #ccc; }') ?>
+<?php  //$this->registerCss('.conteiner{ background: #ccc; }') ?>
+
+<button class="btn btn-success" id='btn'>Click me</button>
+
+<?php 
+
+$js = <<< JS
+		$('#btn').on('click', function(){ 
+			$.ajax({
+				url: 'index.php?r=post/index',
+				data: {test: '123'},
+				type: 'GET',
+				success: function(res){
+					console.log(res);
+				},
+				error:function(){
+					alert('ERROR!');
+				}
+			});
+			
+		 });
+JS;
+
+	$this->registerJs($js);
+
+ ?>
+
