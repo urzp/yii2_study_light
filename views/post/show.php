75ac853cb57693c5ef0b912ec2b76974535f5aee
urzp <urzp000@gmail.com>
2018-01-29 17:51:26 +0300

    Study_7 plag blok of js by registerJs

--- views/post/show.php
+++ views/post/show.php
@@ -1,3 +1,6 @@
 <h1>Show Action</h1>
 
-<?php $this->registerJsFile('@web/js/script.js',['depends' => 'yii\web\YiiAsset']) ?>
+<?php // $this->registerJsFile('@web/js/script.js',['depends' => 'yii\web\YiiAsset']) ?> 
+
+
+<?php $this->registerJs( " $('.conteiner').append('<p>SHOW !!!!</p>'); ") ?>
